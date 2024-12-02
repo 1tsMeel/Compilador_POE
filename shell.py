8261430ccdfbe52e50fe033ae9f25d0be89f4b48
NanoGl <orgonlan@outlook.com>
2024-12-01 18:54:29 -0600

    Cambios y comentarios en shell.py, cambia lo que consideres pertinente MEEL

--- shell.py
+++ shell.py
@@ -1,10 +1,12 @@
 import basic
 
 while True:
-	text = input('basic > ')
+    # Este archivo simulará una consola
+	text = input('introduce > ')
 	if text.strip() == "": continue
-	result, error = basic.run('<stdin>', text)
+	result, error = basic.run('<stdin>', text) # Se ejecuta el comando con <stdin>
 
+	# Si hay error se muestra en pantalla, si no se muestra el resultado
 	if error:
 		print(error.as_string())
 	elif result:
@@ -12,3 +14,15 @@ while True:
 			print(repr(result.elements[0]))
 		else:
 			print(repr(result))
+   
+   
+   # Código para debug descartado
+	""" source_code = """
+	#VAR x = 10
+	#PRINT(x)
+	"""
+	output, error = basic.generate_object_code("<stdin>", source_code, "codigo_objeto.txt")
+	if error:
+		print("Error:", error)
+	else:
+		print(output) """
